47396292980e65eb9bab651f595aba3dc66c4956
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-09 17:46:55 -0800

    created joinTable for author and books tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,24 +1,31 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS books
-DROP TABLE IF EXISTS authors
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS joinTable;
 
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL,
-    released NUMBER
+    released INT
 );
 
 CREATE TABLE authors (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
-    dob YEAR,
-    pob VARCHAR NOT NULL,
+    dob VARCHAR NOT NULL,
+    pob VARCHAR NOT NULL
+);
+
+CREATE TABLE joinTable (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    author_id INT NOT NULL,
+    book_id INT NOT NULL
 );
 
 INSERT INTO books (
     title,
-    released,
+    released
 )
 VALUES
 ('Paroaria gularis', 1998),
@@ -45,7 +52,7 @@ VALUES
 INSERT INTO authors (
     name,
     dob,
-    pob,
+    pob
 )
 VALUES
 ('Clayson Woffenden', '04/30/1967', 'Kiel'),
@@ -68,3 +75,39 @@ VALUES
 ('Marysa Plum', '01/18/1926', 'Lusk'),
 ('Lynsey Joseph', '10/22/1960', 'Nowe Miasto nad Pilicą'),
 ('Wandie Cordeau', '05/13/1925', 'Tvrdonice');
+
+INSERT INTO joinTable (
+    author_id,
+    book_id
+)
+VALUES
+(10, 12),
+(17, 1),
+(11, 10),
+(3, 18),
+(18, 4),
+(4, 7),
+(13, 12),
+(12, 5),
+(8, 16),
+(2, 10),
+(2, 9),
+(1, 18),
+(19, 16),
+(16, 9),
+(15, 1),
+(1, 9),
+(4, 4),
+(3, 10),
+(20, 19),
+(19, 12),
+(8, 8),
+(16, 8),
+(3, 17),
+(15, 20),
+(19, 5),
+(8, 15),
+(13, 11),
+(3, 10),
+(20, 13),
+(8, 20);
