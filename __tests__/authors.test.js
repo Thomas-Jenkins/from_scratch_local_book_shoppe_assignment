e20d75facca605cbbe825373e29c6ca019bccca2
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-10 14:20:37 -0800

    expanded /authors route test to be more specific.

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -9,7 +9,15 @@ describe('authors routes', () => {
   });
   it('should return a list of authors', async () => {
     const res = await request(app).get('/authors');
+    const entryOne = {
+      id: '1',
+      name: 'Clayson Woffenden',
+      dob: '04/30/1967', 
+      pob: 'Kiel'
+    };
     expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0]).toEqual(entryOne);
   });
 
   afterAll(() => {
