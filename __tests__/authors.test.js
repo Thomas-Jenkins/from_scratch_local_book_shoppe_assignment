0d93788284226af0bdfb16134457a36c23666979
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-10 14:15:46 -0800

    second test for /authors route returns all authors.

---
+++ __tests__/authors.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('authors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.status).toBe(200);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
